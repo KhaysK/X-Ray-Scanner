0706324c70008bf113fa5abefbc7767bd81f8046
Khabib <k.khaysadykov@innopolis.university>
2023-04-03 23:42:35 +0300

    Add simple UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="container">
+    <div id="titleInfo">
+        <h1>Lung X-Ray</h1>
+        <div>Determines the presence of lung inflammation by X-ray image</div>
+    </div>
+    <button>Select File</button>
+</div>
+
+<style>
+    #container{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        gap: 96px;
+        align-items: center;
+        background-color: #f3f0ec;
+        color: #22577A;
+        font-size: 24px;
+    }
+    #titleInfo{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;  
+        align-items: center;
+    }
+    button{
+        height: 6rem;
+        width: 22rem;
+        border: none;
+        border-radius: 10px;
+        background-color: #57cc99;
+        color: #f3f0ec;
+        font-size: 36px;
+        transition: all 0.2s ease-in;
+    }
+    button:hover{
+        background-color: #38a3a5;
+    }
+</style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,14 @@
+<footer>
+    Copyright
+</footer>
+
+<style>
+    footer{
+        height: 70px;
+        flex-shrink: 0;
+        text-align: center;
+        line-height: 70px;
+        font-size: 24px;
+        color: #22577A;
+    }
+</style>
