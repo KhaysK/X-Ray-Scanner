4394dd8560f5068da7b3fb12a0aef3f45850a44f
Khabib <k.khaysadykov@innopolis.university>
2023-04-04 02:24:52 +0300

    Fix typo with base

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : './X-Ray-Scanner',
+			base: dev ? '' : '/X-Ray-Scanner',
 		  },
 	}
 };
