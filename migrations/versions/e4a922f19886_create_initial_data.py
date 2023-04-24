a9dc1e0590d3a9cf7d4a8603fdcc9540a626d31a
NodirBobiev <nodir.bobiev@gmail.com>
2023-04-24 23:12:45 +0300

    working version of database

--- migrations/versions/e4a922f19886_create_initial_data.py
+++ migrations/versions/e4a922f19886_create_initial_data.py
@@ -1,34 +1,33 @@
-"""update initial data
+"""create initial data
 
-Revision ID: bf536fadf8bf
-Revises: 53dee3f829ad
-Create Date: 2023-04-24 03:36:29.599601
+Revision ID: e4a922f19886
+Revises: 31b0b281778b
+Create Date: 2023-04-24 23:08:01.286558
 
 """
 from alembic import op
 import sqlalchemy as sa
 from db_models import User
 
-
 # revision identifiers, used by Alembic.
-revision = 'bf536fadf8bf'
-down_revision = '53dee3f829ad'
+revision = 'e4a922f19886'
+down_revision = '31b0b281778b'
 branch_labels = None
 depends_on = None
-
 
 def populate_data():
     data = [
         {'username': 'admin', 'password': 'apass', 'is_admin': True},
+        {'username': 'bdmin', 'password': 'bpass', 'is_admin': True},
         {'username': 'client1', 'password': '1pass', 'is_admin': False},
         {'username': 'client2', 'password': '2pass', 'is_admin': False},
     ]
-    for user_data in data:
-        op.execute(f"UPDATE user SET is_admin = {user_data['is_admin']} WHERE username = '{user_data['username']}'")
-        
+    op.bulk_insert(User.__table__, data)
+
+
 def upgrade():
     populate_data()
-        
+
 
 def downgrade():
     pass
